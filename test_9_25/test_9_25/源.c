559981976675b22edd6a7fefc4238ce39b00c280
wuming12138 <1220691550@qq.com>
2023-06-10 18:13:41 +0800

    test_9_25

---
+++ test_9_25/test_9_25/源.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//		printf("c:\\npc\n");
+//		printf("%d",'\10');
+//	return 0;
+//}
+//int main()
+//{
+//	float  a= 0;
+//	float  b = 0;
+//	scanf("%f %f", &a,&b);
+//	printf("%f %f", a, b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	float c = (a + b) / 2.0;
+//	printf("%f", c);
+//	return 0;
+//}
+int main()
+{
+	int num1 = 0,num2;
+	scanf("%d", num1);
+	num2 = num1+100;
+	printf("%d", num2);
+
+	
+	return 0;
+}
