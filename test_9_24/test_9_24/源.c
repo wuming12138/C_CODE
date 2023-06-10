62c168f284b1afd82bfd4451a00ce5e177c8910e
wuming12138 <1220691550@qq.com>
2023-06-10 18:10:05 +0800

    测试代码

---
+++ test_9_24/test_9_24/源.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//extern int a;
+//int main()
+//{
+//	
+//	printf("%d", a);
+//	return 0;
+//}
+
+//#define MAX 10 
+//int main()
+//{
+//	const int a=10;
+//	int arr[MAX] = { 0 };
+//	return 0;
+//}
+
+//enum sex
+//{
+//	male,
+//	famale,
+//	secret
+//};
+//int main()
+//{
+//	printf("%d\n", male);
+//	printf("%d\n", famale);
+//	printf("%d\n", secret);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = { 'a','b','c','\0'};
+//	printf("%s", arr);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	scanf("%d %d %d",&a,& b,& c);
+//	d = a;
+//	a = c;
+//	c = d;
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
